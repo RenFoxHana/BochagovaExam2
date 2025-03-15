ad6def4be32d9dd0eb327ba97a485d3f4bcc2cbf
VictoriaBochagova <144030378+RenFoxHana@users.noreply.github.com>
2025-03-15 12:40:01 +0300

    Add src program files

---
+++ src/Graph.cs
@@ -0,0 +1,106 @@
+﻿namespace BochagovaExam
+{
+    public class Graph
+    {
+        private int n;
+
+        public Graph(int numberOfVertices)
+        {
+            n = numberOfVertices;
+        }
+
+        public double[,] Floyd(double[,] a)
+        {
+            double[,] d = new double[n, n];
+            d = (double[,])a.Clone();
+            for (int i = 1; i <= n; i++)
+                for (int j = 0; j <= n - 1; j++)
+                    for (int k = 0; k <= n - 1; k++)
+                        if (d[j, k] > d[j, i - 1] + d[i - 1, k])
+                            d[j,k] = d[j,i - 1] + d[i - 1,k];
+            return d;
+        }
+
+        public void FindMinimumPath(double[,] adjacencyMatrix)
+        {
+            Console.WriteLine("Введите номера точек, в которых контейнеры отсутствуют через пробел:");
+            string input = Console.ReadLine();
+            bool[] excludedPoints = new bool[n];
+            foreach (var point in input.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (int.TryParse(point, out int outPoint) && outPoint >= 1 && outPoint <= n)
+                {
+                    excludedPoints[outPoint - 1] = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Точка {point} введена неверно. Она не будет учитываться.");
+                }
+            }
+
+            double[,] distances = Floyd(adjacencyMatrix);
+
+            //Перебор всех точек с исключением пустых
+            int[] remainingPoints = new int[n];
+            int remainingCount = 0;
+            for (int i = 0; i < n; i++)
+                if (!excludedPoints[i])
+                    remainingPoints[remainingCount++] = i;
+
+            if (remainingCount < 2)
+            {
+                Console.WriteLine("Недостаточно точек для построения маршрута.");
+                return;
+            }
+
+            double minPathLength = double.MaxValue;
+            int[] endPath = new int[remainingCount];
+
+            int[] currentPoint = new int[remainingCount];
+            for (int i = 0; i < remainingCount; i++)
+            {
+                currentPoint[i] = remainingPoints[i];
+            }
+
+            //Метод для нахождения пути с пропуском точек с рекурсией
+            void FindPath(int point)
+            {
+                if (point == remainingCount - 1)
+                {
+                    double pathLength = 0;
+                    for (int i = 0; i < remainingCount - 1; i++)
+                    {
+                        pathLength += distances[currentPoint[i], currentPoint[i + 1]];
+                    }
+                    if (pathLength < minPathLength)
+                    {
+                        minPathLength = pathLength;
+                        Array.Copy(currentPoint, endPath, remainingCount);
+                    }
+                }
+                else
+                {
+                    for (int i = point; i < remainingCount; i++)
+                    {
+                        int temp = currentPoint[point];
+                        currentPoint[point] = currentPoint[i];
+                        currentPoint[i] = temp;
+
+                        FindPath(point + 1);
+
+                        temp = currentPoint[point];
+                        currentPoint[point] = currentPoint[i];
+                        currentPoint[i] = temp;
+                    }
+                }
+            }
+
+            FindPath(0);
+
+            Console.Write("Путь: ");
+            foreach (var point in endPath)
+                Console.Write((point + 1) + " ");
+            Console.WriteLine($"Длина пути: {minPathLength:F2}");
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BochagovaExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] adjacencyMatrix = new double[10, 10];
+
+            // Матрица смежности графа
+            adjacencyMatrix = new double[,]
+            {
+                { 0,   0.94, Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue,1.88, Double.MaxValue, Double.MaxValue, Double.MaxValue },
+                { 0.94, 0,   0.66, Double.MaxValue, Double.MaxValue, Double.MaxValue, 1.2,   Double.MaxValue, Double.MaxValue, Double.MaxValue },
+                { Double.MaxValue, 0.66, 0,   1.04, Double.MaxValue, 1.7,   Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue },
+                { Double.MaxValue, Double.MaxValue, 1.04, 0, Double.MaxValue,  0.77,  Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue },
+                { Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue, 0,   1.92, Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue },
+                { Double.MaxValue, Double.MaxValue, 1.7,   0.77, 1.92, 0, Double.MaxValue, Double.MaxValue, Double.MaxValue,  1.52 },
+                { 1.88, 1.2,   Double.MaxValue,   Double.MaxValue, Double.MaxValue, Double.MaxValue, 0,   0.53, Double.MaxValue, Double.MaxValue },
+                { Double.MaxValue, Double.MaxValue,   Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue, 0.53, 0,   1.54, Double.MaxValue },
+                { Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue, 1.54, 0,     0.86},
+                { Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue, Double.MaxValue, 1.52, Double.MaxValue, Double.MaxValue, 0.86, 0 }
+            };
+
+            int numberOfVertices = adjacencyMatrix.GetLength(0);
+            Graph graph = new Graph(numberOfVertices);
+
+            graph.FindMinimumPath(adjacencyMatrix);
+        }
+    }
+}
